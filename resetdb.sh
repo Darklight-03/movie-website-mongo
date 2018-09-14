80124ff552ce5ec319b3168ae3536e4623350a27
Bo Corman <bdc3142@gmail.com>
2018-09-14 14:29:41 -0500

    fixed resetdb syntax

--- resetdb.sh
+++ resetdb.sh
@@ -1,4 +1,4 @@
-password = `cat rwuser-passwd`
+password=`cat rwuser-passwd`
 
 mongo movies_mongo -u rwuser -p $password --eval 'db.movies.drop()'
 mongo movies_mongo -u rwuser -p $password --eval 'db.credits.drop()'
