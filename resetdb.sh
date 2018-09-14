3eda358ffe2abf9da303454ec8e332cb01f09b35
Bo Corman <bdc3142@gmail.com>
2018-09-14 14:26:46 -0500

    added resetdb.sh for deleting and then reimporting data to the database

---
+++ resetdb.sh
@@ -0,0 +1,5 @@
+password = `cat rwuser-passwd`
+
+mongo movies_mongo -u rwuser -p $password --eval 'db.movies.drop()'
+mongo movies_mongo -u rwuser -p $password --eval 'db.credits.drop()'
+/bin/bash populatedb.sh
