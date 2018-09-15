db096230858f73b810805fa3f9c7ae04ba9b8f7e
Bo Corman <bdc3142@gmail.com>
2018-09-15 14:47:57 -0500

    fixed syntax getMovieStats

--- commands.js
+++ commands.js
@@ -28,15 +28,17 @@ db.system.js.save(
   {
     _id: "getMovieStats",
     value: function(x) {
-      count = db.runCommand({ count: "movies" })
+      count = db.runCommand({ count: "movies" });
       total_runtime = db.movies.group(
-        key: {runtime: 1},
-        initial: {total: 0},
-        $reduce: function(curr,result){
-          result.total += curr.runtime;
+        {
+          key: {runtime: 1},
+          initial: {total: 0},
+          $reduce: function(curr,result){
+            result.total += curr.runtime;
+          }
         }
       )
-      return total_runtime
+      return total_runtime;
     }
   }
 )
