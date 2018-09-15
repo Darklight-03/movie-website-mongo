f2c2cf1ab95abfc1c83d89133fc75766a6dd19fc
Bo Corman <bdc3142@gmail.com>
2018-09-15 23:44:42 +0000

    Creates credits field but they are null

--- populatedb.sh
+++ populatedb.sh
@@ -2,4 +2,4 @@ password=`cat rwuser-passwd`
 
 #mongoimport --db movies_mongo --collection movies --mode upsert --username rwuser --password $password --file dataJSON/movies.json --jsonArray
 #mongoimport --db movies_mongo --collection movies --mode upsert --username rwuser --password $password --file dataJSON/credits.json --jsonArray
-mongo movies_mongo --username user --password fghg readCredits.js
+mongo movies_mongo --username rwuser --password $password readCredits.js
--- readCredits.js
+++ readCredits.js
@@ -1,11 +1,20 @@
+
+//db.movies.updateMany( { "hasCredits" : 1 }, { $set { "credits": db.credits.findOne( { "id" : ??? } ) } } );
+
 var moviesCursor = db.movies.find( { "hasCredits" : 1 } );
 
-
-moviesCursor.forEach( function (doc) {
-	var movieid = doc.id;
-	print(movieid);
-	print(db.credits.find( { "id" : movieid } ));
-	doc.credits = db.credits.find( { "id" : movieid } );
+moviesCursor.forEach( function (currentMovie) {
+	print(currentMovie.hasCredits);
+	if (db.credits.findOne( { "id" : currentMovie.id } ) != null) {
+		db.movies.updateOne( { "_id" : currentMovie._id }, { $set: { "credits" : db.credits.findOne( { "id" : currentMovie.id } ) } } );
+		print("Credits updated " + currentMovie.id);
+	}
+	else {
+		print("No credits " + currentMovie.id);
+	}
+	
+	// doesn't actually update anything
+	//currentMovie.credits = ...;
 })
 
 moviesCursor.close();
