71c1552419fe229518f0bd7eb00b8613372efb37
wts923 <walter.stager@tamu.edu>
2018-09-16 16:04:31 -0500

    Full scale test with progress messages

--- readPersons.js
+++ readPersons.js
@@ -3,12 +3,15 @@ print("Creating persons collection...");
 
 
 // much slower method that works
-var moviesCursor = db.movies.find( { "id": "862" } );
+var moviesCursor = db.movies.find( { "credits": { $ne: null } } );
+var numMovies = db.movies.count();
+var moviesIterated = 0;
+
 moviesCursor.forEach( function (currentMovie) {
 	var i;
-	print("sz=" + currentMovie.credits.cast.length);
+	//print("sz=" + currentMovie.credits.cast.length);
 	for (i = 0; i < currentMovie.credits.cast.length; i++) {
-		print(currentMovie.credits.cast[i].name);
+		//print(currentMovie.credits.cast[i].name);
 		db.persons.updateOne(
 			{ "id": {$eq: currentMovie.credits.cast[i].id } },
 			{ $set: { "name": currentMovie.credits.cast[i].name,
@@ -20,9 +23,21 @@ moviesCursor.forEach( function (currentMovie) {
 		);
 	}
 	for (i = 0; i < currentMovie.credits.crew.length; i++) {
-		
+		//print(currentMovie.credits.cast[i].name);
+		db.persons.updateOne(
+			{ "id": {$eq: currentMovie.credits.cast[i].id } },
+			{ $set: { "name": currentMovie.credits.cast[i].name,
+			         "gender": currentMovie.credits.cast[i].gender,
+					 "profile_path": currentMovie.credits.cast[i].profile_path },
+			  $addToSet: { "crewMovies": currentMovie._id }
+			},
+			{ upsert: 1 }
+		);
 	}
-
+	moviesIterated += 1;
+	if (moviesIterated % 1000) {
+		print("Progress: " + (100 * (moviesIterated/numMovies)) + "%");
+	}
 });
 moviesCursor.close();
 
