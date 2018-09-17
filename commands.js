d9b24abbf9bad5ba8c660d643c4e9314e0d374e4
Bo Corman <bdc3142@gmail.com>
2018-09-16 22:14:46 -0500

    fixed typo?

--- commands.js
+++ commands.js
@@ -77,26 +77,24 @@ db.system.js.save(
     _id: "getCreditsStats",
     value: function(x) {
       count = db.credits_ids.count();
-      numcast = db.persons.aggregate([
+      
+      numcast = db.persons.aggregate([ 
         {
-          $match: {
-            is_cast: {$eq: true}
-          }
+          $match: {is_cast: {$eq: 1}} 
         },
         {
           $count: "cast"
-        }
+        } 
       ]).toArray()[0].cast;
-      numcrew = db.persons.aggregate([
+      numcrew = db.persons.aggregate([ 
         {
-          $match: {
-            is_crew: {$eq: true}
-          }
+          $match: {is_crew: {$eq: 1}} 
         },
         {
           $count: "crew"
         }
       ]).toArray()[0].crew;
+
       return `- Credits Entries: ${count}\n- Cast Members: ${numcast}\n- Crew Members: ${numcrew}`;
     }
   }
