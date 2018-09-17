bb3f98fb1de391a05009d0c222e7a03063dd34fa
Wilson Kahlich <w_kahlich@tamu.edu>
2018-09-16 20:49:16 -0500

    last try

--- commands.js
+++ commands.js
@@ -87,7 +87,7 @@ db.system.js.save(
     {
         _id: "printStats",
         value: function(x) {
-            return printjson(x.explain("executionStats"));
+            return x.explain("executionStats");
         }
     }
 )
