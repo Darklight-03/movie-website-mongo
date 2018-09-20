f40b6a70bda73d6398e25cd58ef0e2e82eb8b6d2
김승진 <kimsj1993@gmail.com>
2018-09-20 11:31:47 -0500

    Update Model of movie in angular

--- website/angular-src/src/app/movie/movie.model.ts
+++ website/angular-src/src/app/movie/movie.model.ts
@@ -1,16 +1,25 @@
+import {Person} from '../person/person.model';
+
 export class Movie {
   public id: number;
   public title: string;
+  public original_language: string;
+  public overview: string;
+  public popularity: number;
   public posterPath: string;
-  public castIDList: number[];
-  public crewIDList: number[];
+  public castList: Person[];
+  public crewList: Person[];
 
 
-  constructor(id: number, title: string, posterPath: string, castIDList: number[], crewIDList: number[]) {
+  constructor(id: number, title: string, original_language: string, overview: string,
+              popularity: number, posterPath: string, castList: Person[], crewList: Person[]) {
     this.id = id;
     this.title = title;
+    this.original_language = original_language;
+    this.overview = overview;
+    this.popularity = popularity;
     this.posterPath = posterPath;
-    this.castIDList = castIDList;
-    this.crewIDList = crewIDList;
+    this.castList = castList;
+    this.crewList = crewList;
   }
 }
