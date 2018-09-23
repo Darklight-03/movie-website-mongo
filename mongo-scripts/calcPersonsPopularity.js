59ec6c895a588def6dc190dd756a92a9a83f92cd
wts923 <wts923@tamu.edu>
2018-09-23 21:54:20 +0000

    Wasn't actually updating the db

--- mongo-scripts/calcPersonsPopularity.js
+++ mongo-scripts/calcPersonsPopularity.js
@@ -2,8 +2,8 @@ print("Calculating popularity...");
 
 db.movies.reIndex();
 
-var personsCursor = db.persons.find().noCursorTimeout();
-var numPersons = db.persons.count();
+var personsCursor = db.persons.find( { "cast_movies": { $exists: 1 } } ).noCursorTimeout();
+var numPersons = db.persons.count(  {"cast_movies":  { $exists: 1 } } );
 var personsIterated = 0;
 
 
@@ -11,19 +11,24 @@ personsCursor.forEach( function (currPerson) {
 	var totalPopularity = 0;
 	var numMovies = 0;
 	
-	for (i = 0; i < currPerson.cast_movies; i++) {
+	for (i = 0; i < currPerson.cast_movies.length; i++) {
 		totalPopularity += db.movies.findOne( { "_id": currPerson.cast_movies[i]._id }, { "popularity":1 } ).popularity;
 		numMovies++;
 	}
 
-	currPerson.popularity = (totalPopularity / numMovies);
+	db.persons.updateOne( { "_id": currPerson._id }, { $set: { "popularity": (totalPopularity / numMovies) } } );
+	//currPerson.popularity = (totalPopularity / numMovies);
 
 	if ((personsIterated % 30000) == 0) {
-		print("Progress: ~" + (100 * (personsIterated/numPersons)).toString().match(/^-?\d+(?:\.\d{0,2})?/)[0] + "%.  " + personsIterated + "/" + numPersons + " movies");
-		print("pop: " + pcurrPerson.popularity);
-		print("also: " + (totalPopularity / numMovies);
+		print("Progress: ~" + (100 * (personsIterated / numPersons)).toString().match(/^-?\d+(?:\.\d{0,2})?/)[0] + "%.  " + personsIterated + "/" + numPersons + " movies");
 	}
 	
-	moviesIterated += 1;
+
+	personsIterated += 1;
 });
 
+
+db.persons.updateMany(
+	{ "popularity": { $exists: 0 } },
+	{ $set: { "popularity": 0 } }
+);
