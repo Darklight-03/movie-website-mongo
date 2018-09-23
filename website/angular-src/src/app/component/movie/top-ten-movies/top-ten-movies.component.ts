b0272651e54fb29cd56bfa1e7a5a37e1df1ad95e
김승진 <kimsj1993@gmail.com>
2018-09-22 22:24:07 -0500

    Implement homage with top ten grossing movies

--- website/angular-src/src/app/component/movie/top-ten-movies/top-ten-movies.component.ts
+++ website/angular-src/src/app/component/movie/top-ten-movies/top-ten-movies.component.ts
@@ -9,28 +9,19 @@ import {MovieService} from '../../services/movie.service';
 })
 export class TopTenMoviesComponent implements OnInit {
 
-  topMovies: MoviePoster[] = [
-    new MoviePoster(5, 'dssdlkasdlajdasldasldjasljdalsjdlasjdalsa', 'This is funny', 15,
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUKYsL-HsONvskWHVxg3YgwYKgp6aPzEOdench8tCnSxiBOpO1'),
-    new MoviePoster(6, 'dssdlkasdlajdasldasldjasljdalsjdlasjdalsa', 'This is funny', 15,
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUKYsL-HsONvskWHVxg3YgwYKgp6aPzEOdench8tCnSxiBOpO1'),
-    new MoviePoster(6, 'dssdlkasdlajdasldasldjasljdalsjdlasjdalsa', 'This is funny', 15,
-      'https://www.vintagemovieposters.co.uk/wp-content/uploads/2016/04/IMG_1392.jpg'),
-    new MoviePoster(6, 'dssdlkasdlajdasldasldjasljdalsjdlasjdalsa', 'This is funny', 15,
-      'https://www.vintagemovieposters.co.uk/wp-content/uploads/2016/04/IMG_1392.jpg'),
-    new MoviePoster(6, 'dssdlkasdlajdasldasldjasljdalsjdlasjdalsa', 'This is funny', 15,
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUKYsL-HsONvskWHVxg3YgwYKgp6aPzEOdench8tCnSxiBOpO1'),
-    new MoviePoster(6, 'dssdlkasdlajdasldasldjasljdalsjdlasjdalsa', 'This is funny', 15,
-      'https://www.vintagemovieposters.co.uk/wp-content/uploads/2016/04/IMG_1392.jpg'),
-    new MoviePoster(6, 'dssdlkasdlajdasldasldjasljdalsjdlasjdalsa', 'This is funny', 15,
-      'https://www.vintagemovieposters.co.uk/wp-content/uploads/2016/04/IMG_1392.jpg')
-
-  ];
+  topMovies: MoviePoster[] = [];
 
     constructor(private  listServ: MovieService) {}
 
   ngOnInit() {
-       // this.listServ.getMovie(2).subscribe((data: object) => {
-      // }) ;
+
+    this.listServ.getTopGrossing().subscribe((data: Object) => {
+      console.log(data);
+      for (let i = 0; i < 10; i++) {
+        this.topMovies.push(new MoviePoster(data[i]['id'], data[i]['title'],
+          data[i]['overview'], data[i]['popularity'], data[i]['poster_path']));
+      }
+      console.log(this.topMovies);
+    });
   }
 }
