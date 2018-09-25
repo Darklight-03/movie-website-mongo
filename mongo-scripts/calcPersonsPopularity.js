702312020603490587602ec0e43ac85c138e520d
wts923 <wts923@tamu.edu>
2018-09-25 16:46:52 -0500

    first draft of popularity weighting

--- mongo-scripts/calcPersonsPopularity.js
+++ mongo-scripts/calcPersonsPopularity.js
@@ -10,17 +10,22 @@ var personsIterated = 0;
 personsCursor.forEach( function (currPerson) {
 	var totalPopularity = 0;
 	var numMovies = 0;
-	
+	var oldPop = currPerson.popularity;
+
 	for (i = 0; i < currPerson.cast_movies.length; i++) {
 		totalPopularity += db.movies.findOne( { "_id": currPerson.cast_movies[i]._id }, { "popularity":1 } ).popularity;
 		numMovies++;
 	}
-
-	db.persons.updateOne( { "_id": currPerson._id }, { $set: { "popularity": (totalPopularity / numMovies) } } );
+	
+	totalPopularity = (0.3 * (totalPopularity / numMovies)) + numMovies;
+	
+	db.persons.updateOne( { "_id": currPerson._id }, { $set: { "popularity": totalPopularity } } );
 	//currPerson.popularity = (totalPopularity / numMovies);
 
 	if ((personsIterated % 30000) == 0) {
-		print("Progress: ~" + (100 * (personsIterated / numPersons)).toString().match(/^-?\d+(?:\.\d{0,2})?/)[0] + "%.  " + personsIterated + "/" + numPersons + " movies");
+		print("Progress: ~" + (100 * (personsIterated / numPersons)).toString().match(/^-?\d+(?:\.\d{0,2})?/)[0] + "%.  " + personsIterated + "/" + numPersons + " people");
+		print(oldPop);
+		print(totalPopularity);
 	}
 	
 
@@ -32,3 +37,6 @@ db.persons.updateMany(
 	{ "popularity": { $exists: 0 } },
 	{ $set: { "popularity": 0 } }
 );
+
+
+print("Done calculating popularity.");
