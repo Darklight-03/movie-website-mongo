5ce6f894142b041fa1c1ba4b0d78b86ba7af912f
wts923 <wts923@tamu.edu>
2018-09-29 14:43:08 -0500

    Added search collection, js to create and modified pop/reset scripts

--- mongo-scripts/resetdb.sh
+++ mongo-scripts/resetdb.sh
@@ -4,4 +4,5 @@ mongo movies_mongo -u rwuser -p $password --eval 'db.movies.drop()'
 mongo movies_mongo -u rwuser -p $password --eval 'db.credits.drop()'
 mongo movies_mongo -u rwuser -p $password --eval 'db.credits_ids.drop()'
 mongo movies_mongo -u rwuser -p $password --eval 'db.persons.drop()'
+mongo movies_mongo -u rwuser -p $password --eval 'db.global_search.drop()'
 /bin/bash populatedb.sh
