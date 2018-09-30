ea7a286399e201bd83d430425c97c6b4e744aae9
wts923 <wts923@tamu.edu>
2018-09-30 15:42:19 -0500

    Trying to manually merge with P1D3

--- website/angular-src/src/app/services/authentication.service.ts
+++ website/angular-src/src/app/services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import {User} from '../model/user.model';
 
 @Injectable()
 export class AuthenticationService {
@@ -26,4 +27,59 @@ export class AuthenticationService {
     // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
   }
+
+  // movies
+   public getMovie( id: number ) {
+     const URL = `${this.apiUrl}/movie?id=${id}`;
+     return this.http.get(URL);
+   }
+
+   public getTopGrossing() {
+     const URL = `${this.apiUrl}/topgrossing`;
+     return this.http.get(URL);
+   }
+
+   public getPopularMovies() {
+     const URL = `${this.apiUrl}/popularmovies`;
+     return this.http.get(URL);
+   }
+
+
+   // search
+   public getSearchResults( q: string ) {
+     const URL = `${this.apiUrl}/search?q=${q}`;
+     return this.http.get(URL);
+   }
+
+
+   // person
+   public getPerson( id: number ) {
+     const URL = `${this.apiUrl}/person?id=${id}`;
+     return this.http.get(URL);
+   }
+
+
+   public getPopularPeople() {
+     const URL = `${this.apiUrl}/popularpeople`;
+     return this.http.get(URL);
+   }
+
+
+   // user
+   getUserById(id: number) {
+     return this.http.get(`${this.apiUrl}/users?id=${id}`);
+   }
+
+   registerUser(user: User) {
+     return this.http.post(`${this.apiUrl}/users/register`, user);
+   }
+
+   deleteUser(id: number) {
+     return this.http.delete(`${this.apiUrl}/users?id=${id}`);
+   }
+
+   public autoComplete(q: string) {
+     const URL = `${this.apiUrl}/autocomplete?q=${q}`;
+     return this.http.get(URL);
+   }
 }
