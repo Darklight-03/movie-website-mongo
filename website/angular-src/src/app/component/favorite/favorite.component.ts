54f97f57358bc6b0b9874c7e905ebcc58bbf906f
김승진 <kimsj1993@gmail.com>
2018-10-01 02:11:52 -0500

    finished favorite page

--- website/angular-src/src/app/component/favorite/favorite.component.ts
+++ website/angular-src/src/app/component/favorite/favorite.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NetworkService} from '../../services/network.service';
 import {Movie} from '../../model/movie.model';
+import {AlertService} from '../../services/alert.service';
 
 @Component({
   selector: 'app-favorite',
@@ -10,7 +11,7 @@ import {Movie} from '../../model/movie.model';
 export class FavoriteComponent implements OnInit {
   movies: Movie[] = [];
 
-  constructor(private service: NetworkService) { }
+  constructor(private service: NetworkService, private alertService: AlertService) { }
 
   ngOnInit() {
     this.service.getFavorite(this.service.getUserDetails()['_id']).subscribe((movies: Object) => {
@@ -19,6 +20,13 @@ export class FavoriteComponent implements OnInit {
         this.movies.push(new Movie(movies[i]['id'], movies[i]['title'], null, null, null, 0, movies[i]['poster_path'], null, null, null ));
         i++;
       }
+    });
+  }
+
+  onRemove(id: number) {
+    this.service.removeFavorite(this.service.getUserDetails()['_id'], id).subscribe((data: Object) => {
+      alert('The movie is removed from your favorites');
+      location.reload();
     });
   }
 
